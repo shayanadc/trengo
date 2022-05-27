40d6b163a29bdf81c9627d9083a132bbd7caec27
shayan <shayanadc@gmail.com>
2022-05-27 19:49:36 +0430

    add category relation in article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,7 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
     protected $fillable = ['title', 'body'];
+
     protected $hidden = ['created_at', 'updated_at'];
 
+    /**
+     * The roles that belong to the user.
+     */
+    public function categories(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
