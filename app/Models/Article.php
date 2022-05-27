fd5c422a472a9f642b04be33cc287e44fa5d8224
shayan <shayanadc@gmail.com>
2022-05-27 11:49:06 +0430

    add article model factory seeder migration

---
+++ app/Models/Article.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    protected $hidden = ['created_at', 'updated_at'];
+}
