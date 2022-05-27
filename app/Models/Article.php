b748ffecec10d6f197e3f4bb112f2ce698108773
shayan <shayanadc@gmail.com>
2022-05-27 13:10:15 +0430

    add fillable attributes for article mass assignment

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+    protected $fillable = ['title', 'body'];
     protected $hidden = ['created_at', 'updated_at'];
+
 }
