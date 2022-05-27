4be152a5f9708e40a3880240638a6fc4f32910cb
shayan <shayanadc@gmail.com>
2022-05-27 19:51:33 +0430

    add method adding category

--- app/Services/Article/Concretes/ArticleStoreService.php
+++ app/Services/Article/Concretes/ArticleStoreService.php
@@ -3,13 +3,29 @@
 namespace App\Services\Article\Concretes;
 
 use App\Models\Article;
+use App\Models\Category;
 use App\Services\Article\Contracts\ArticleStoreContract;
+use Illuminate\Support\Collection;
 
 class ArticleStoreService implements ArticleStoreContract
 {
-    public function store(array $array) : Article
+    public function store(array $body) : Article
     {
-        return Article::create($array);
+        $categoryIds = $body['categories'] ?? [];
+        $categoryIds = static::getManyCategoryIds($categoryIds);
+
+        return static::createArticleAndAttachCategories($body, $categoryIds);
+
+    }
+    public static function getManyCategoryIds(array $ids): Collection
+    {
+        return Category::existed($ids)->get('id')->pluck('id');
+    }
+
+    public static function createArticleAndAttachCategories($articleAttributes, $categoryIds){
+        $article = Article::create($articleAttributes);
+        $article->categories()->attach($categoryIds);
+        return $article;
     }
 
 }
