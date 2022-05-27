e33c70095dc2cc8e502ec4933c8fdb5f738c3458
shayan <shayanadc@gmail.com>
2022-05-27 13:32:00 +0430

    add string validation for request

--- app/Http/Requests/StoreArticleRequest.php
+++ app/Http/Requests/StoreArticleRequest.php
@@ -24,7 +24,8 @@ class StoreArticleRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required'
+            'title' => 'required|string',
+            'body' => 'string'
         ];
     }
 }
