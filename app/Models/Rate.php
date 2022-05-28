9d5b0b5869057a10ffcd2863d13aa51f6e44d27b
shayan <shayanadc@gmail.com>
2022-05-28 19:09:55 +0430

    Add rate model

---
+++ app/Models/Rate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rate extends Model
+{
+    use HasFactory;
+    protected $hidden = ['created_at', 'updated_at'];
+    protected $fillable = ['rate', 'article_id', 'ip'];
+}
