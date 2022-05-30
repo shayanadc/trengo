4c8f67a71013644ac4c26e3da273f4871466b132
shayan <shayanadc@gmail.com>
2022-05-30 12:30:08 +0430

    add view model

--- app/Models/RealTimeView.php
+++ app/Models/RealTimeView.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,27 @@ class RealTimeView extends Model
     use HasFactory;
 
     protected $fillable = ['article_id', 'ip'];
+
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeGroupByArticle($query): \Illuminate\Database\Eloquent\Builder
+    {
+        return $query->groupBy('article_id')
+            ->selectRaw('count(*) as count, article_id');
+    }
+
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeViewedAt($query, $date): \Illuminate\Database\Eloquent\Builder
+    {
+        return $query->whereDay('created_at', $date);
+    }
 }
