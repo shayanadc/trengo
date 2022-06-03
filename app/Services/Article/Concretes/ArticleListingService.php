cf382ed258212084bea2f41d5a38721c1028682e
shayan <shayanadc@gmail.com>
2022-06-03 09:39:47 +0430

    refactor apply filter to query

--- app/Services/Article/Concretes/ArticleListingService.php
+++ app/Services/Article/Concretes/ArticleListingService.php
@@ -12,13 +12,17 @@ use App\Query\Article\ArticleView;
 use App\Services\Article\Contracts\ArticleListingContract;
 use Illuminate\Pipeline\Pipeline;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\DB;
 
 class ArticleListingService implements ArticleListingContract
 {
     public function getMany(): Collection
     {
-        $pipeline = app(Pipeline::class)
+        return $this->applyFilters()->get();
+    }
+
+    public function applyFilters()
+    {
+        return app(Pipeline::class)
             ->send(Article::query())
             ->through([
                 ArticleTitle::class,
@@ -28,6 +32,5 @@ class ArticleListingService implements ArticleListingContract
                 ArticleView::class,
                 ArticleRate::class
             ])->thenReturn();
-        return $pipeline->get();
     }
 }
